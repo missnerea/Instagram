0eb1de90a96ec0a6350806aac4b7ba66a6cc1336
missnerea <ela.amish@gmail.com>
2018-03-08 11:03:07 +0300

    Add login required to views

--- thegram/gramygram/views.py
+++ thegram/gramygram/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
     title='Welcome to Instagram'
 
