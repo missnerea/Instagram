e6c9b19c5fbe2ff46a9d3121d87d294b21cc7b9e
missnerea <ela.amish@gmail.com>
2018-03-09 12:29:20 +0300

    add class model

--- thegram/gramygram/models.py
+++ thegram/gramygram/models.py
@@ -5,3 +5,8 @@ class User(models.Model):
     username=models.CharField(max_length=20)
     email=models.CharField(max_length=60)
     password = models.CharField(max_length=80)
+
+class Image(models.Model):
+    image =models.ImageField(upload_to='/thegram',blank=True)
+    caption=models.CharField(max_length=100)
+    
