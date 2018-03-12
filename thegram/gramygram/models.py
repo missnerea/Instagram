190516797cc912ff381eb24a007638a3ca3aa49e
missnerea <ela.amish@gmail.com>
2018-03-12 05:12:41 +0300

    add image likes to Image model

--- thegram/gramygram/models.py
+++ thegram/gramygram/models.py
@@ -11,6 +11,7 @@ class Image(models.Model):
     user = models.ForeignKey(User,null = True)
     image_image =models.ImageField(upload_to='images/',blank=True)
     caption=models.CharField(max_length=100)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True,related_name='image_likes')
          
     @classmethod
     def this_image(cls):
